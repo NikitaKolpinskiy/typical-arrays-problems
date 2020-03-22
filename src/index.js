2c3d1fbf56480302cd47f0dcefcf1b5d0876d080
NikitaKolpinskiy <nikitakolpinski@mail.ru>
2020-03-22 13:13:25 +0300

    Feat: solution to typical array problem, this task can be smaller if u using ternary operator

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array && array.length) {
+    array.sort((a,b) => a-b);
+    return array[0];
+  } else {
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array && array.length) {
+    array.sort((a,b) => a-b);
+    return array[array.length -1];
+  } else {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array && array.length) {
+    return array.reduce((prev, curr) => prev + curr) / array.length;
+  } else {
+    return 0;
+  }
 }
